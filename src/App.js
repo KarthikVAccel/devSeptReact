4cbb743577327a41617be3c15fbede70db8d8cf7
KarthikVAccel <karthikraj@v-accel.ai>
2025-11-05 14:16:49 +0530

    commit todo

--- src/App.js
+++ src/App.js
@@ -1,64 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Counter from "./continer/counter";
+import TodoLocal from "./continer/todo";
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 
 
-function App() {
-  let [todo, setTodo] = useState([]);
-  let [limit,setLimit] = useState(2);
-  let [skip,setSkip] = useState(0);
-  
+let App = () =>{
 
-  useEffect(() => {
-    console.log("second  type");
-  });
-
-  useEffect(() => {
-    console.log("third type");
-  }, [todo]);
-
-  return (
-    <div className="App">
-      {
-        todo.map((item)=>{
-          return (
-            <div key={item.id}>
-              <h3>{item.todo}</h3>
-              <p>Completed: {item.completed ? "Yes" : "No"}</p>
-              <p>User ID: {item.userId}</p>
-              <p>Todo ID: {item.id}</p>
-              <hr />
-              </div>
-          )
-        })
-      }
-<hr></hr>
-
-Limit : <input type="number" placeholder="limit" value={limit} onChange={(e)=>setLimit(e.target.value)} />
-<br/>
-Skip: <input type="number" placeholder="skip" value={skip} onChange={(e)=>setSkip(e.target.value)} /><br/>
-<button onClick={()=>{
- axios.get(`https://dummyjson.com/todos?limit=${limit}&skip=${skip}`).then((res)=>{
-  setTodo(res.data.todos);
- }).catch((err)=>{
-  console.log(err); 
-})
-}}>Fetch Todos</button>
-<button onClick={()=>{
- axios.post(`https://dummyjson.com/todos/add`,{
-    todo: 'Use DummyJSON in the project',
-    completed: false,
-    userId: 5,
-  }).then((res)=>{
-  setTodo([res.data]);
- }).catch((err)=>{
-  console.log(err); 
-})
-}}>Add Todos</button>
-     
+  return(
+    <div>
+       <Counter></Counter>
+       <hr></hr>
+       <TodoLocal />
     </div>
-  );
+  )
 }
 
 export default App;
