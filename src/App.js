c61b885b7639a063cdeb0d88d931edda20972d55
KarthikVAccel <karthikraj@v-accel.ai>
2025-11-17 15:20:20 +0530

    commit protected route

--- src/App.js
+++ src/App.js
@@ -4,15 +4,34 @@ import Counter from "./container/counter";
 import TodoLocal from "./container/todo";
 import TodoOnline from "./container/todoOnline";
 import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+
+let Protected = ({children}) =>{
+    let isAuth = true
+
+    return isAuth ? children : <Navigate to="/login" />;
+
+}
 
 
-let App = () =>{
-
-  return(
-    <div>
-       <TodoOnline />
-    </div>
-  )
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Counter</Link> <br/>
+         <Link to="/todoLocal">TodoLocal</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Counter />} />
+         <Route path="/login" element={
+          <>
+            <p> You donot have access</p>
+          </>
+         } />
+        <Route path="/todoLocal" element={<Protected> <TodoLocal/> </Protected>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
